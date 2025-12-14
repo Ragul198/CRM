a0abbd27b44926971547134bdbbd90b495e1c61e
Ragul198 <ragul198a@gmail.com>
2025-12-14 23:25:48 +0530

    com

--- CRM-SMIE/frontend/src/api/axiosInstance.jsx
+++ CRM-SMIE/frontend/src/api/axiosInstance.jsx
@@ -1,8 +1,8 @@
-import axios from 'axios'
+import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5001/api',
-    withCredentials: true
-})
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
+  withCredentials: true,
+});
 
 export default axiosInstance;
--- CRM-SMIE/frontend/src/api/socketIO.jsx
+++ CRM-SMIE/frontend/src/api/socketIO.jsx
@@ -1,15 +1,16 @@
-// socket.js
 import { io } from "socket.io-client";
 
 let socket;
 
 export const initSocket = (userId, role) => {
-  socket = io("http://localhost:5001", {
+  socket = io(import.meta.env.VITE_BACKEND_URL, {
     auth: { userId, role },
   });
+
   socket.on("connect", () => {
     console.log("Connected to socket server with ID:", socket.id);
   });
+
   return socket;
 };
 
